f0c9ffaa4b02fb43a70f29f557531d15855c5567
aris abdulgani <aris.gani@gmail.com>
2021-09-27 10:38:38 +0400

    ticketing CRUD tests

--- Ticketting/src/Routes/__test__/TicketCRUD.test.ts
+++ Ticketting/src/Routes/__test__/TicketCRUD.test.ts
@@ -16,4 +16,26 @@ it("check to post a new ticket and get it using the id",async()=>{
 it("check to get a ticket with an incorrect id",async()=>{
     return await request(app).get('/api/ticketing/123123123123').set("Cookie",generateSession("aris.gani@gmail.com","1234")).expect(500)
 })
+it("test to get all tickets",async()=>{
+    await request(app).post('/api/ticketing').set("Cookie",generateSession("aris.gani@gmail.com","1234")).send({
+        title:"neeeew",price:123
+    }).expect(201)
+    await request(app).post('/api/ticketing').set("Cookie",generateSession("aris.gani@gmail.com","1234")).send({
+        title:"neeeew",price:123
+    }).expect(201)
+    await request(app).post('/api/ticketing').set("Cookie",generateSession("aris.gani@gmail.com","1234")).send({
+        title:"neeeew",price:123
+    }).expect(201)
+    let allTickets=await request(app).get('/api/ticketing').set("Cookie",generateSession("aris.gani@gmail.com","1234")).expect(201)
+    expect(allTickets.body.length).toBe(3)
+})
 
+it("update ticket test",async()=>{
+    let ticket=await request(app).post('/api/ticketing').set("Cookie",generateSession("aris.gani@gmail.com","1234")).send({
+        title:"neeeew",price:123
+    }).expect(201)
+    let response = await request(app).post('/api/ticketing/update').set("Cookie",generateSession("aris.gani@gmail.com","1234")).send({
+        title:"neeeew1",price:123, _id:ticket.body._id
+    }).expect(201)
+    expect(response.body.title).toBe("neeeew1")
+})
